f322b84b8d4712dfc35a644f0bdfe9c1fa6307b4
Jacob Foster Davis <jfosterdavis@users.noreply.github.com>
2018-01-27 19:15:13 -0500

    Added skeleton for working with InteractiveSideMenu

--- Example/Pods/Target Support Files/Pods-ZMAppFoundation_Example/Pods-ZMAppFoundation_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-ZMAppFoundation_Example/Pods-ZMAppFoundation_Example-frameworks.sh
@@ -135,10 +135,12 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GaugeKit/GaugeKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/InteractiveSideMenu/InteractiveSideMenu.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ZMAppFoundation/ZMAppFoundation.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GaugeKit/GaugeKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/InteractiveSideMenu/InteractiveSideMenu.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ZMAppFoundation/ZMAppFoundation.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
