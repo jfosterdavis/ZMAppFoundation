5bbc72b93f40f5d55b5ecf20ba898ca6b360fedc
Jacob Foster Davis <jfosterdavis@users.noreply.github.com>
2018-02-03 14:17:29 -0500

    Still designing the menu cells with Xibs, but changed the tableview controller and tableview to be just a single subclassed tableviewcontroller

--- ZMAppFoundation/Classes/TableViewCells/ZMMenuTableCell.swift
+++ ZMAppFoundation/Classes/TableViewCells/ZMMenuTableCell.swift
@@ -8,10 +8,10 @@
 import Foundation
 import UIKit
 
+@IBDesignable
 class ZMMenuTableCell: UITableViewCell {
     
     @IBOutlet weak var menuItemLabel: UILabel!
-    
     
     func loadCell(from menuItem: ZMMenuItem) {
         menuItemLabel.text = menuItem.displayTitle
--- ZMAppFoundation/Classes/ZMMenuTableViewController.swift
+++ ZMAppFoundation/Classes/ZMMenuTableViewController.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
-class ZMMenuTableViewController: UIViewController {
+@IBDesignable
+class ZMMenuTableViewController: UITableViewController {
 
-    @IBOutlet weak var menuTableView: ZMMenuTableView!
+    //@IBOutlet weak var menuTableView: ZMMenuTableView!
     
     let tableCellId = "ZMMenuTableCell"
     let tableCellNibName = "ZMMenuTableCell"
@@ -18,19 +19,44 @@ class ZMMenuTableViewController: UIViewController {
         super.viewDidLoad()
         
         // Delegate the TableView
-        self.menuTableView.delegate = self
-        self.menuTableView.dataSource = self
+        //self.menuTableView.delegate = self
+        //self.menuTableView.dataSource = self
         
         // Set the bundle
         let bundle = Bundle(for: type(of: self))
         
         // Register table cell class from nib
         let cellNib = UINib(nibName: tableCellNibName, bundle: bundle)
-        self.menuTableView.register(cellNib, forCellReuseIdentifier: self.tableCellId)
+        self.tableView.register(cellNib, forCellReuseIdentifier: self.tableCellId)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    /******************************************************/
+    /*******************///MARK: Delegate and DataSource
+    /******************************************************/
+
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ZMMenuItems.StandardItems.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: tableCellId, for: indexPath as IndexPath) as! ZMMenuTableCell
+        
+        let menuItem = ZMMenuItems.StandardItems[indexPath.row]
+        
+        cell.loadCell(from: menuItem)
+        
+        return cell
+    }
+    
+    override public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let menuItem = ZMMenuItems.StandardItems[indexPath.row]
+        
+        menuItem.itemSelected()
     }
 }
